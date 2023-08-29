436fab15a2346fd565fe38ecd8fc5584e6a6e678
Ted30 <teddymarcenaje@gmail.com>
2023-08-29 23:04:13 +0800

    added additional functions

--- controllers/room-controllers.js
+++ controllers/room-controllers.js
@@ -1,4 +1,5 @@
 const Room = require('../models/room-schema')
+const Message = require('../models/message-schema')
 
 const getRooms = async (req, res, next) => {
     let rooms
@@ -12,29 +13,71 @@ const getRooms = async (req, res, next) => {
     })
 }
 
-const createRoom = async (req, res, next) => {
-    const { name, user_id, recipient_id } = req.params
+const getRoomDetails = async (req, res, next) => {
+    let room
+    try {
+        room = await Room.findOne({ _id: req.params.room_id })
+    } catch (err) {
+        console.log(err)
+    }
+    res.json(room)
+}
 
-    const room = new Room({
-        name,
-        participants: [
+getRoomMessages = async (req, res, next) => {
+    let chats
+    try {
+        chats = await Message.find({ room_id: req.params.room_id, status: 1 })
+        res.json(chats.map((chat) => chat.toObject({ getters: true })))
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+getRoomLastMessage = async (req, res, next) => {
+    let chats
+    try {
+        chats = await Message.find({ room_id: req.params.room_id, status: 1 })
+            .sort({ createdAt: -1 })
+            .limit(1)
+        res.json(chats.map((chat) => chat.toObject({ getters: true })))
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+getRoomMessagesCount = async (req, res, next) => {
+    let chats
+
+    try {
+        chats = await Message.find({
+            room_id: req.params.room_id,
+            sender_id: { $ne: req.params.user_id },
+            read: false,
+            status: 1,
+        }).count()
+        res.json({ count: chats })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+readMessages = async (req, res, next) => {
+    try {
+        await Message.updateMany(
             {
-                user_id,
+                room_id: req.params.room_id,
+                sender_id: { $ne: req.body.user_id },
             },
-            {
-                recipient_id,
-            },
-        ],
-    })
-
-    const response = await room.save()
-
-    res.status(201).json({
-        message: 'Room Created',
-        car: room,
-    })
-    this.socket.emit('new-room-created', { room })
+            { $set: { read: true } }
+        )
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 exports.getRooms = getRooms
-exports.createRoom = createRoom
+exports.getRoomDetails = getRoomDetails
+exports.getRoomMessages = getRoomMessages
+exports.getRoomLastMessage = getRoomLastMessage
+exports.getRoomMessagesCount = getRoomMessagesCount
+exports.readMessages = readMessages
