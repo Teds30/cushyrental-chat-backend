a0820d46cd879b0298fe287799eba6856f442cbf
Ted30 <teddymarcenaje@gmail.com>
2023-08-29 23:04:31 +0800

    added seen function

--- controllers/socket-message-controller.js
+++ controllers/socket-message-controller.js
@@ -2,17 +2,32 @@ const BaseController = require('./BaseController')
 const Message = require('../models/message-schema')
 
 module.exports = class MessageController extends BaseController {
-    sendMessage = ({ message, sender_id, room_id }) => {
+    sendMessage = ({ message, sender_id, is_system, type, room_id }) => {
         const msg = new Message({
             message,
             sender_id,
+            is_system,
             room_id,
+            type,
+            createdAt: new Date().toISOString(),
         })
 
-        // msg.save()
+        msg.save()
 
-        this.socket.emit('message-sent', { message: msg })
+        // this.socket.emit('message-sent', { message: msg })
         let skt = this.socket.broadcast
         skt.to(room_id).emit('message-sent', { message: msg })
+    }
+
+    receiverSeen = async ({ room_id }) => {
+        await Message.updateMany(
+            {
+                room_id: room_id,
+            },
+            { $set: { read: true } }
+        )
+
+        let skt = this.socket.broadcast
+        skt.to(room_id).emit('receiver-seen')
     }
 }
