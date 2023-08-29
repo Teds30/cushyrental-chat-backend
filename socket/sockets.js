c8b849664be26b68ad2f12830ceb6a7ccd36f632
Ted30 <teddymarcenaje@gmail.com>
2023-08-29 23:06:32 +0800

    added room leave, seen & uploading image function

--- socket/sockets.js
+++ socket/sockets.js
@@ -1,6 +1,7 @@
 const RoomController = require('../controllers/socket-room-controller')
 const MessageController = require('../controllers/socket-message-controller')
 const TypingController = require('../controllers/socket-typing-controller')
+const fs = require('fs')
 
 const sockets = (socket) => {
     console.log('connected')
@@ -9,6 +10,7 @@ const sockets = (socket) => {
     const typingController = new TypingController(socket)
 
     socket.on('room-join', roomController.joinRoom)
+    socket.on('room-leave', roomController.leaveRoom)
     socket.on('room-create', roomController.createRoom)
     socket.on('room-remove', roomController.roomRemoved)
     socket.on('unit-avail', roomController.availUnit)
@@ -17,6 +19,18 @@ const sockets = (socket) => {
     socket.on('typing-stop', typingController.typingStopped)
 
     socket.on('send-message', messageController.sendMessage)
+    socket.on('receiver-seen-signal', messageController.receiverSeen)
+
+    socket.on('upload', ({ data, room_id }) => {
+        fs.writeFile(
+            'upload/' + 'test.png',
+            data,
+            { encoding: 'base64' },
+            () => {}
+        )
+        socket.to(room_id).emit('uploaded', { buffer: data.toString('base64') })
+        socket.emit('uploaded', { buffer: data.toString('base64') })
+    })
 
     socket.on('disconnect', (socket) => {
         console.log('User left.')
